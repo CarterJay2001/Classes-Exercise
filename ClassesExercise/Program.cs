12d7f8e64e26d0009d7d1e9971b0c81c6566d913
Jaylon Carter <carterjay2001@outlook.com>
2022-01-19 14:58:26 -0600

    initial commit

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var DreamCar = new Car();
+            DreamCar.Make = "Telsa";
+            DreamCar.Model = "Model S Plaid";
+            DreamCar.Year = 2022;
+
+            Console.WriteLine(DreamCar.Make);
+            Console.WriteLine(DreamCar.Model);
+            Console.WriteLine(DreamCar.Year);
+
+            var LikeCar = new Car();
+            LikeCar.Make = "Jeep";
+            LikeCar.Model = "Wrangler Rubicon 392";
+            LikeCar.Year = 2022;
+
+            Console.WriteLine(LikeCar.Make);
+            Console.WriteLine(LikeCar.Model);
+            Console.WriteLine(LikeCar.Year);
+        }
+    }
+}
